dbdbff91f88a38369abb0df57e56c69b63e8e1eb
rbichaev <32089411+rbichaev@users.noreply.github.com>
2020-05-11 21:48:56 +0300

    Upload files

--- 09/main.cpp
+++ 09/main.cpp
@@ -1,27 +1,35 @@
 #include <iostream>
 #include <string>
+#include <array>
 #include "threadsort.h"
 
-int main()
+// выводим на экран номер теста
+void print_test()
 {
-    ThreadSort alg("proba.txt", 13, 5);
-    if (!alg.sort())
-        std::cout << "ПРИ РАБОТЕ С ФАЙЛАМИ ПРОИЗОШЛА ОШИБКА" << std::endl;
+    static int test_num = 0;
+    test_num += 1;
+    std::cout << "------" << std::endl;
+    std::cout << "ТЕСТ " << test_num << std::endl;
+    std::cout << "------" << std::endl;
+}
 
-    uint64_t nums[] = {0, 1, 2, 3, 4, 5, 8, 12, 12, 32, 45, 67, 322};
+void check_result(const std::string &filename, const std::vector<uint64_t> nums)
+{
+    print_test();
 
     std::ifstream stream;
     std::string str;
 
     bool mistake = false;
 
-    stream.open("result.txt");
-    for (size_t i=0; i<13; i++)
+    stream.open(filename);
+    for (size_t i=0; i<nums.size(); i++)
     {
         getline(stream, str);
         if (std::stoi(str) != nums[i])
         {
             std::cout << "WRONG!" << std::endl;
+            mistake = true;
             break;
         }
     }
@@ -30,4 +38,29 @@ int main()
         std::cout << "Well done!" << std::endl;
 
     stream.close();
+}
+
+int main()
+{
+    // ТЕСТ_1
+    ThreadSort alg1("vals1.txt", 13, 5);
+    if (!alg1.sort())
+        std::cout << "ПРИ РАБОТЕ С ФАЙЛАМИ ПРОИЗОШЛА ОШИБКА" << std::endl;
+    else
+    {
+        std::vector<uint64_t> nums1 = {0, 1, 2, 3, 4, 5, 8, 12, 12, 32, 45, 67, 322};
+        check_result("result.txt", nums1);
+    }
+
+    remove("result.txt");
+
+    // ТЕСТ_2
+    ThreadSort alg2("vals2.txt", 20, 5);
+    if (!alg2.sort())
+        std::cout << "ПРИ РАБОТЕ С ФАЙЛАМИ ПРОИЗОШЛА ОШИБКА" << std::endl;
+    else
+    {
+        std::vector<uint64_t> nums2 = {0, 0, 2, 2, 2, 2, 3, 4, 6, 8, 9, 9, 53, 56, 66, 78, 134, 230, 234, 4578};
+        check_result("result.txt", nums2);
+    }
 }
